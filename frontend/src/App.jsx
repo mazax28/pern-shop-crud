9aff310ed49397c1fe61e71bb0ba23e88e4749f1
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-06-05 11:42:37 -0300

    feat: add ProductsPage route to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,21 @@ import { BrowserRouter,Routes,Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import Navbar from "./components/Navbar"
 import ProductPage from "./pages/ProductPage"
-import { useThemeStore } from "./store/useThemeStore"
 import {Toaster} from 'react-hot-toast'
 import Footer from "./components/Footer"
+import ProductsPage from "./pages/ProductsPage"
 
 function App() {
 
-  const {theme} = useThemeStore()
   return (
     <BrowserRouter>
-    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme} >
+    <div className="min-h-screen bg-gray-50 transition-colors duration-300" >
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
+
+
           <Route path="/product/:id" element={<ProductPage />} />
 
         </Routes>
